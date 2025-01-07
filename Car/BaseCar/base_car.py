8976f735229b54f2686d8b7fbd8da234448eed05
Hendrik <hendrik.zilich@volkswagen.de>
2025-01-07 15:13:44 +0100

    Implement Base Car Class

--- Car/BaseCar/base_car.py
+++ Car/BaseCar/base_car.py
@@ -18,9 +18,53 @@ from basisklassen import PWM
 
 class BaseCar:
     def __init__(self):
-        self.steering_angle = None
-        self.speed = None
-        self.direction = None
+        self._steering_angle = None
+        self._speed = None
+        self.direction = 0
+        self.backwheels = BackWheels()
+        self.frontwheels = FrontWheels()
+
+    @property
+    def steering_angle(self):
+        return self._steering_angle
+    
+    @steering_angle.setter
+    def steering_angle(self,value):
+        self._steering_angle=value
+        if value < 45:
+            self._steering_angle = 45
+        if value > 135:
+            self._steering_angle = 135
+
+
+    @property
+    def speed(self):
+        return self._speed
+    
+    @speed.setter
+    def speed(self,value):
+        self._speed=value
+        if value < -100:
+            self._speed = -100
+        if value > 100:
+            self._speed = 100
+
+    def drive(self):
+        self.backwheels.speed(self._speed)
+        self.frontwheels.turn(self._steering_angle)
+        if self._speed < 0:
+            self.backwheels.backward()
+            self.direction= -1
+        else:
+            self.backwheels.forward()
+            self.direction= 1 
+    
+    def stop(self):
+        self._speed= 0
+        self.backwheels.stop()
+        self.direction= 0
 
 basecar = BaseCar()
+print(basecar.steering_angle)
+basecar.steering_angle=120
 print(basecar.steering_angle)
