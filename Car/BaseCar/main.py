b8c2ec6ae15d041b2ce9e22c5ddca8bf93169956
DucProgramming <Duc_Vo@live.de>
2025-01-08 23:09:19 +0100

    foo

---
+++ Car/BaseCar/main.py
@@ -0,0 +1,4 @@
+from base_car import BaseCar
+
+basecar = BaseCar()
+basecar.mode_driving_1(45)
---
+++ Car/BaseCar/sonic_car.py
@@ -0,0 +1,8 @@
+import click
+import csv
+import json
+from pathlib import Path
+from datetime import datetime
+from datetime import timezone
+from base_car import BaseCar
+from basisklassen import Ultrasonic
