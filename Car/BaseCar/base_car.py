a6e63ecd548b6a9a2af107ecf00628a8762b42b2
DucProgramming <Duc_Vo@live.de>
2025-01-08 08:37:03 +0100

    Update base_car

--- Car/BaseCar/base_car.py
+++ Car/BaseCar/base_car.py
@@ -5,6 +5,9 @@ import math
 import RPi.GPIO as GPIO
 import smbus
 import json
+import pdb
+import os
+from pathlib import Path
 
 
 from basisklassen import Ultrasonic
@@ -22,12 +25,16 @@ class BaseCar:
         self._speed = 0
         self.direction = 0
         self.backwheels = BackWheels()
-        self.frontwheels = FrontWheels()
+        path = Path(__file__).parents[0].joinpath("config.json")
+        with open(path, "r") as f:
+                data = json.load(f)
+                turning_offset = data["turning_offset"]
+        self.frontwheels = FrontWheels(turning_offset=turning_offset)
 
     @property
     def steering_angle(self):
         return self._steering_angle
-    
+
     @steering_angle.setter
     def steering_angle(self,value):
         self._steering_angle=value
@@ -36,11 +43,10 @@ class BaseCar:
         if value > 135:
             self._steering_angle = 135
 
-
     @property
     def speed(self):
         return self._speed
-    
+
     @speed.setter
     def speed(self,value):
         self._speed=value
@@ -49,29 +55,124 @@ class BaseCar:
         if value > 100:
             self._speed = 100
 
-    def drive(self):
-        #self.backwheels.speed(self._speed)
-        self.frontwheels.turn(self._steering_angle)
-        self.backwheels.speed = abs(self.speed)  # Methods backwards and forwards are accessing on backwheels.speed which is on default 0
-        if self._speed < 0:
-            self.backwheels.backward()
-            self.direction = -1
-        else:
-            self.backwheels.forward()
-            self.direction= 1 
-    
-    def stop(self):
-        self._speed = 0
-        self.backwheels.stop()
-        self.direction = 0
+    def drive(self, new_val_steering_angle = None, new_val_speed = None ):
+        if new_val_steering_angle:
+            self.steering_angle = new_val_steering_angle
 
-def main():
+        if new_val_speed:
+            self.speed = new_val_speed
+
+        self.frontwheels.turn(self._steering_angle)
+        print(f"Current value of steering_angle: {self.steering_angle}")
+
+        self.backwheels.speed = abs(self.speed)
+        if self.speed < 0:
+            self.backwheels.forward()
+            self.direction = -1
+            print(f"Current value of speed: {self.speed}")
+        else:
+            self.backwheels.backward()
+            self.direction= 1
+            print(f"Current value of speed: {self.speed}")
+
+    def stop(self):
+        self.speed = 0
+        self.backwheels.stop()
+
+    def mode_driving_1(self):
+        self.speed = 30
+        self.steering_angle = 90
+        time.sleep(0.5)
+
+        self.drive()
+        time.sleep(3)
+
+        self.stop()
+        time.sleep(0.5)
+
+        self.speed = -30
+        self.drive()
+        time.sleep(3)
+
+        self.stop()
+
+    def mode_driving_2(self):
+        self.speed = 30
+        self.steering_angle = 90
+        time.sleep(0.5)
+
+        self.drive()
+        time.sleep(1)
+
+        self.stop()
+        self.speed = 30
+        self.steering_angle = 135
+        time.sleep(0.5)
+
+        self.drive()
+        time.sleep(8)
+
+        self.stop()
+        time.sleep(0.5)
+
+        self.speed = -30
+        self.drive()
+        time.sleep(8)
+
+        self.stop()
+        time.sleep(0.5)
+
+        self.speed = 30
+        self.steering_angle = 45
+        self.drive()
+        time.sleep(8)
+
+        self.stop()
+        time.sleep(0.5)
+
+        self.speed = -30
+        self.drive()
+        time.sleep(8)
+
+        self.steering_angle = 90
+        self.stop()
+
+
+
+@click.command()
+@click.option('--modus', '--m', type=int, default=None, help="Auswahl Fahrtmodus")
+def main(modus):
+    modi = {
+        1: '3s Geradeausfahrt, 1s Pause, 3s Rückwärtsfahrt',
+        2: '1s Geradeausfahrt, 8s im Uhrzeigersinn + zurück, 8s gegen Uhrzeigersinn + zurück'
+        }
+
+    if modus == None:
+        print('----' * 20)
+        print('Auswahl:')
+        for m in modi.keys():
+            print(f"{m} - {modi[m]}")
+        print('----' * 20)
+
+    while modus == None:
+        try:
+            modus_list = list(modi.keys())
+            modus = int(input('Wähle  (Andere Taste für Abbruch): ? '))
+            break
+        except:
+            print('Getroffene Auswahl nicht möglich.')
+            quit()
     basecar = BaseCar()
-    basecar.speed = 30
-    basecar.steering_angle = 135
-    basecar.drive()
-    time.sleep(2)
-    basecar.stop()
+    if modus == 1:
+        basecar.mode_driving_1()
+
+    if modus == 2:
+        basecar.mode_driving_2()
+
+    foo = Infrared()
+    while True:
+        print(foo.read_analog())
+        time.sleep(2)
 
 
 
