1111e22f555b8f3af9be9545e7865ff9eeec5e59
bjber <b.bernholt@gmx.de>
2025-01-13 14:33:07 +0100

    arbeitsstand dropdown

--- DashApp/graphics.py
+++ DashApp/graphics.py
@@ -2,9 +2,15 @@ from dash import Dash, html, Dash, html, dash_table, dcc, callback, Output, Inpu
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
+import csv
 
 # Incorporate data
-df = pd.read_csv("C:\\Users\\bbern\\Downloads\\log.csv")
+path = "C:\\Users\\bbern\\Downloads\\log.csv"
+with open(path, "r", encoding="unicode_escape") as f:
+            reader = csv.DictReader(f)
+            erste_zeile = next(reader) # Erste Zeile lesen 
+            options_list = list(erste_zeile.values())
+print(options_list)
 
 # Initialize the app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -17,14 +23,17 @@ app.layout = [
     dbc.Row([
     dbc.Col(dcc.Dropdown(
         id='my_dropdown',
-        options=[
-            {"label": "Speed", "value" : "speed"},
-            {"label": "Steering angle", "value": "steering_angle"},
-            {"label": "Direction", "value": "direction"},
-            {"label": "Ultrasonicsensor", "value": "distance_ahead"},
-        ],
+        options=options_list
+            # {"label": "Speed", "value" : "speed"},
+            # {"label": "Steering angle", "value": "steering_angle"},
+            # {"label": "Direction", "value": "direction"},
+            # {"label": "Ultrasonicsensor", "value": "distance_ahead"},
+        ,
         value="speed")),  # Standardwert
     dbc.Col(dcc.Graph(figure={}, id='my_graph'), xs=12, md=12),
+    dbc.Button("Load latest Data", id="my_button"),
+    dcc.Markdown(children="", id = "output_div"),
+    dcc.Markdown("fieldnames")
 ])
     
 ]
@@ -42,6 +51,13 @@ def update_graph(value): # the function argument comes from the component proper
                        }
     figure=px.line(df, x='time', y=value, labels={"time": "time [s]", value: possible_values[value]}) 
     return figure # the returned object is assigned to the component property of the Output
+
+@callback(
+    Output("output_div", "children"),
+    Input(component_id='my_button', component_property='n_clicks')
+)
+def update_log_data(n_clicks):
+    return f"Der Button wurde {n_clicks} mal geklickt."
 
 # Run the app
 if __name__ == '__main__':
