def53b1c5a569f1388e74b27a57730378703e11e
Viviane Wolters <vivi.wolters@gmail.com>
2025-01-14 13:32:44 +0100

    adjust layout, calculate driving time

--- DashApp/dash_app.py
+++ DashApp/dash_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pathlib import Path
 import plotly.express as px
 import dash_bootstrap_components as dbc
+import plotly.graph_objects as go
 
 app = Dash(
          __name__, external_stylesheets=[
@@ -11,13 +12,6 @@ app = Dash(
             ],
             suppress_callback_exceptions=True 
     )
-
-#Dummy Fahrzeugdaten
-speed_min = 0
-speed_max = 100
-speed_mean = 50
-driving_time = 10
-driving_distance = 500
 
 # Logdaten einlesen 
 def read_log_data():
@@ -29,43 +23,66 @@ def read_log_data():
 def log_data_to_dicts(df):
     return df.to_dict('records')
 
-def create_kpi_card(card_id, card_color, header, value):
+# Layout Components
+
+# Gauge
+def create_gauge(value, color):
+    fig = go.Figure(go.Indicator(
+        mode="gauge+number",
+        value=value,
+        domain={'x': [0, 1], 'y': [0, 1]},
+        gauge={'axis': {'range': [None, 100]}, 'bar': {'color': f"#{color}" }},
+    ))
+    fig.update_layout(
+        margin=dict(l=10, r=10, t=30, b=30),  
+        font=dict(size=12),  
+        height=150,          
+        width=200            
+    )
+    return fig
+
+#Card
+def create_kpi_card(card_id, card_color, header, gauge=False):
     card_class_name = "card-" + card_color
     header_class_name = "card-header-" + card_color
-
+    card_body = html.H1(0.0, id=f"{card_id}-value")
+    if  gauge == True:
+        card_body = dcc.Graph(
+                    figure=create_gauge(0.0, card_color),
+                    id=f"{card_id}-value",
+                )
     card_content = [
         dbc.CardHeader(header, class_name=header_class_name),
         dbc.CardBody(
             [
-                html.H5(value, id=f"{card_id}-value")
+                card_body
             ],
-            className="d-flex justify-content-center align-items-center" 
+            className="d-flex justify-content-center align-items-center",
         ),
     ]
 
     card = dbc.Card(card_content, className=card_class_name + " h-100", id=card_id)
-
     return card
-
+  
 #Navbar
 navbar = dbc.Navbar(
     dbc.Container(
         [
            html.A(
-                html.Img(src="/assets/autonomes-auto.png", height="50px", className="navbar-logo")
+                html.Img(src="/assets/car.svg", height="50px", className="navbar-logo")
             ),
             dbc.Nav(
                 [
-                    dbc.NavItem(dbc.NavLink("Dashboard", href="#", active=True)),
-                    dbc.NavItem(dbc.NavLink("Car", href="#")),
+                    dbc.NavItem(dbc.NavLink("Dashboard", href="Dashboard", active=True)),
+                    dbc.NavItem(dbc.NavLink("Car", href="Car")),
                 ],
                 navbar=True,
                 className="me-auto",  # Links linksbündig
             ),
         ],
-        fluid=True  # Container über die volle Breite
+        fluid=True 
     ),
-    dark=True,
+    dark= True,
     className= "custom-navbar"
 )
 
@@ -74,20 +91,33 @@ app.layout = html.Div(
     dbc.Stack(
         [
             navbar,
-            dbc.Button("Logdaten einlesen", id="load-log-button", n_clicks=0),
-            dbc.Stack(
+            dcc.Location(id='url', refresh=False),
+            html.Div(id='page-content', className="main-container"),  
+        ]
+    )
+)
+
+# Callback für das Laden des richtigen Inhalts basierend auf der URL
+@app.callback(
+    Output('page-content', 'children'),
+    [Input('url', 'pathname')]  # Input ist die aktuelle URL
+)
+
+def display_page(pathname):
+    if pathname is None or pathname == '/' or pathname =='/Dashboard':
+        content = dbc.Stack(
                 [
-                
                     dbc.Row(
                         [
-                            dbc.Col(create_kpi_card("card-speed-min", "pink", "Geschwindigkeit (Min)", speed_min), className="align-items-stretch"),
-                            dbc.Col(create_kpi_card("card-speed-max", "green", "Geschwindigkeit (Max)", speed_max), className="align-items-stretch"),
-                            dbc.Col(create_kpi_card("card-speed-mean", "orange", "Geschwindigkeit (Mean)", speed_mean), className="align-items-stretch"),
-                            dbc.Col(create_kpi_card("card-driving-distance", "yellow", "Zurückgelegte Strecke", driving_distance), className="align-items-stretch"),
-                            dbc.Col(create_kpi_card("card-driving-time", "blue", "Fahrzeit", driving_time), className="align-items-stretch"),
+                            dbc.Col(create_kpi_card("card-speed-min", "e9c46b", "Geschwindigkeit (Min)", True), className="align-items-stretch"),
+                            dbc.Col(create_kpi_card("card-speed-max", "e66f51", "Geschwindigkeit (Max)", True), className="align-items-stretch"),
+                            dbc.Col(create_kpi_card("card-speed-mean", "f3a261", "Geschwindigkeit (Mean)", True), className="align-items-stretch"),
+                            dbc.Col(create_kpi_card("card-driving-distance", "2a9d8e", "Zurückgelegte Strecke"), className="align-items-stretch"),
+                            dbc.Col(create_kpi_card("card-driving-time", "264553", "Fahrzeit"), className="align-items-stretch"),
                         ],
                         className="row-cols-5 g-3"
                     ),
+                    dbc.Button("Logdaten einlesen", id="load-log-button", n_clicks=0, className="custom-btn"),
                     html.Div(
                         dbc.Stack(
                             [
@@ -102,16 +132,15 @@ app.layout = html.Div(
                     )
                 ],
                 gap=5,
-                className="main-container"
             )
-        ]
-    )
-)
+        return content
+    elif pathname == '/Car':
+        return html.Div([ html.H3("Car Content")])
 
 @app.callback(
-    [Output("card-speed-min-value", "children"),
-     Output("card-speed-max-value", "children"),
-     Output("card-speed-mean-value", "children"),
+    [Output("card-speed-min-value", "figure"),
+     Output("card-speed-max-value", "figure"),
+     Output("card-speed-mean-value", "figure"),
      Output("card-driving-distance-value", "children"),
      Output("card-driving-time-value", "children")],
     [Input("load-log-button", "n_clicks")]
@@ -119,14 +148,28 @@ app.layout = html.Div(
 def update_log_data(n_clicks):
     if n_clicks > 0:
         log_data_df = read_log_data()
-        speed_min_log = round(log_data_df['speed'].min(), 1)
+
+        speeds_greater_than_zero = log_data_df['speed'][log_data_df['speed'] > 0]
+        if not speeds_greater_than_zero.empty:
+            speed_min_log = round(speeds_greater_than_zero.min(), 1)
+        else:
+            speed_min_log = 0.0
+
         speed_max_log = round(log_data_df['speed'].max(), 1)
         speed_mean_log = round(log_data_df['speed'].mean(), 1)
         driving_distance_log = round((log_data_df['speed'] * log_data_df['time']).sum(), 1)
-        driving_time_log = round(log_data_df['time'].sum(), 1)
-        return [html.H5(speed_min_log), html.H5(speed_max_log), html.H5(speed_mean_log), html.H5(driving_distance_log), html.H5(driving_time_log)]
-    
-    return ["0.0", "0.0", "0.0", "0.0", "0.0"]
+        driving_time_log = round(log_data_df.iloc[-1]['time'] - log_data_df.iloc[0]['time'], 1)
 
+        return [
+                create_gauge(speed_min_log, "e9c46b"),
+                create_gauge(speed_max_log, "e66f51"), 
+                create_gauge(speed_mean_log, "f3a261"), 
+                html.H5(driving_distance_log),
+                html.H5(driving_time_log)
+            ]
+    
+    return [create_gauge(0.0, "e9c46b"), create_gauge(0.0, "e66f51"), create_gauge(0.0, "f3a261"), "0.0", "0.0"]
+
+   
 if __name__ == '__main__':
     app.run_server(debug=True, host="0.0.0.0", port=8051)
